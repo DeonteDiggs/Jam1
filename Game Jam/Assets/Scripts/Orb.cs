b830b5bfd55128382d90238c430030ddd983e84a
AngeloDiggs86 <71779228+AngeloDiggs86@users.noreply.github.com>
2021-02-04 03:08:37 -0400

    Add files via upload

---
+++ Game Jam/Assets/Scripts/Orb.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class Orb : MonoBehaviour
+{
+    ParticleSystem particleSystem;
+  
+    SpriteRenderer sprite;
+
+    MenuManager menuManager;
+
+    int maxOrbCount = 0;
+    static int counter = 0;
+
+    private void Start()
+    {
+        menuManager = FindObjectOfType<MenuManager>();
+
+        maxOrbCount = FindObjectsOfType<Orb>().Length;
+
+        particleSystem = GetComponent<ParticleSystem>();
+
+        sprite = GetComponent<SpriteRenderer>();
+
+        particleSystem.Stop();
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+       if(other.gameObject.tag == "Player")
+       {
+            particleSystem.Play();
+
+            sprite.enabled = false;
+
+            Destroy(this, 1);
+
+            counter++;
+            
+            menuManager.SetScore(counter);
+
+            if (counter == maxOrbCount)
+                GoalActivation();
+       }
+    }
+
+    void GoalActivation()
+    {
+        print("Victory");
+    }
+
+}
---
+++ Game Jam/Assets/Scripts/Save.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Save : MonoBehaviour
+{
+    [SerializeField] GameObject Player;
+    MenuManager menuManager;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        menuManager = FindObjectOfType<MenuManager>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            int xPos = PlayerPrefs.GetInt("Integer");
+
+            Player.transform.position = new Vector3(xPos, Player.transform.position.y, Player.transform.position.z);
+
+            float playerPositionX = PlayerPrefs.GetFloat("PlayerPositionX");
+            float playerPositionY = PlayerPrefs.GetFloat("PlayerPositionY");
+            float playerPositionZ = PlayerPrefs.GetFloat("PlayerPositionZ");
+            Vector3 position = new Vector3(playerPositionX, playerPositionY, playerPositionZ);
+            Player.transform.position = position;
+
+
+            int score = PlayerPrefs.GetInt("Score");
+            
+        }
+            
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+            CheckPoint(other.transform.position);    
+    }
+
+    void CheckPoint(Vector3 playerPosition)
+    {
+        PlayerPrefs.SetFloat("PlayerPositionX", playerPosition.x);
+        PlayerPrefs.SetFloat("PlayerPositionY", playerPosition.y);
+        PlayerPrefs.SetFloat("PlayerPositionZ", playerPosition.z);
+        PlayerPrefs.Save();
+    }
+    public static void SaveData(int score)
+    {
+        PlayerPrefs.SetInt("Score", score);
+        PlayerPrefs.Save();
+    }
+}
